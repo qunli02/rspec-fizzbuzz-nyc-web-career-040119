cc2ca39790f1cc1723d5dc3c644a86780c34cfa4
Qun Li Huang <qunli02@gmail.com>
2019-03-20 05:22:23 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,10 +2,10 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0 
-    return "fizzbuzz"
-  elsif int % 3 == 10
+    return "FizzBuzz"
+  elsif int % 3 == 0
     return "Fizz"
-    
-    
+  elsif int % 5 == 0
+    return "Buzz"
   end
 end
