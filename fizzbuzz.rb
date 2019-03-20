5b57fce7e1144f61d8d1a3a6ade8c13ea3647bdc
Qun Li Huang <qunli02@gmail.com>
2019-03-20 05:21:18 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,4 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz
+def fizzbuzz(int)
+  if int % 3 == 0 && int % 5 == 0 
+    return "fizzbuzz"
+  elsif int % 3 == 10
+    return "Fizz"
+    
+    
+  end
 end
